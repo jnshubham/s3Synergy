25c40dd72aaf664d568ab94d1ecc3e5e6d2a10bd
Shubham Jain <43238222+jnshubham@users.noreply.github.com>
2020-04-28 15:35:05 +0530

    Added folder querying capabilities
    Added capability to query on a folder containing multiple files.

--- flask_main.py
+++ flask_main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, url_for, request, make_response, session
 import os
-# os.environ["AWS_SHARED_CREDENTIALS_FILE"] = r"C:\Flask\s3DataSampler\creds\aws_credentials.csv"
 import utils
 from s3Synergy import S3Synergy
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='089bcc9bf633533dd60b6f19402d1634'
@@ -15,25 +15,35 @@ def hello_world():
 @app.route('/', methods=['GET','POST'])
 def get_data():
     args = utils.generate_kwargs(request.form)
-    df, scanned_bytes, processed_bytes = S3Synergy().readData(**args)
-    #df = pd.read_csv(r"C:\Flask\s3DataSampler\sample\sample_csv.csv")
+    if(request.form['optionsRadios']=='file'):
+        df, scanned_bytes, processed_bytes = S3Synergy().readData(**args)
+    elif(request.form['optionsRadios']=='folder'):
+        args['wildcard']=request.form['wildcard']
+        df, scanned_bytes, processed_bytes = S3Synergy().readDataFromFolder(**args)
+    # df = pd.read_csv(r"C:\Flask\s3DataSampler\sample\sample_csv.csv")
     path = os.path.dirname(os.path.realpath(__file__))+r'\tmp\test.csv'
     try:
         os.remove(path)
     except Exception:
         pass
+    start = datetime.datetime.now()
     df.to_csv(path,index=False)
+    print('savingTime')
+    print(datetime.datetime.now()-start)
     session['op_file']=path
     bytes_data = '''<div class="card-body">
     Bytes Scanned:{scanned_bytes}
     Bytes Processed: {processed_bytes}</div>'''.format(scanned_bytes=scanned_bytes,processed_bytes=processed_bytes)
-    strs=df.to_html(index=False)
+    strs=df.head(200).to_html(index=False)
     strs=strs.replace('<table border="1" class="dataframe">','<table class="table table-bordered table-hover table-sm">').replace('<thead>','<thead class="thead-dark">').replace('<th>','<th scope="col">').replace('<tr style="text-align: right;">','')
     return render_template('s3_synergy_home.html', data=strs, dropdown=utils.download_dropdown(), bytes_data=bytes_data)
 
 @app.route('/download/<dtype>', methods=['GET','POST'])
 def download_data(dtype):
+    start=datetime.datetime.now()
     df = pd.read_csv(session['op_file'])
+    print('readingTime')
+    print(datetime.datetime.now()-start)
     if(dtype=='csv'):
         resp = make_response(df.to_csv(index=False))
         resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
--- s3Synergy.py
+++ s3Synergy.py
@@ -10,7 +10,8 @@ class S3Synergy:
     '''Fetch the data based on the key provided and returns the output'''
     def __init__(self):
         self._reserved_keywords = ['absolute', 'action', 'add', 'all', 'allocate', 'alter', 'and', 'any', 'are', 'as', 'asc', 'assertion', 'at', 'authorization', 'avg', 'bag', 'begin', 'between', 'bit', 'bit_length', 'blob', 'bool', 'boolean', 'both', 'by', 'cascade', 'cascaded', 'case', 'cast', 'catalog', 'char', 'char_length', 'character', 'character_length', 'check', 'clob', 'close', 'coalesce', 'collate', 'collation', 'column', 'commit', 'connect', 'connection', 'constraint', 'constraints', 'continue', 'convert', 'corresponding', 'count', 'create', 'cross', 'current', 'current_date', 'current_time', 'current_timestamp', 'current_user', 'cursor', 'date', 'day', 'deallocate', 'dec', 'decimal', 'declare', 'default', 'deferrable', 'deferred', 'delete', 'desc', 'describe', 'descriptor', 'diagnostics', 'disconnect', 'distinct', 'domain', 'double', 'drop', 'else', 'end', 'end-exec', 'escape', 'except', 'exception', 'exec', 'execute', 'exists', 'external', 'extract', 'false', 'fetch', 'first', 'float', 'for', 'foreign', 'found', 'from', 'full', 'get', 'global', 'go', 'goto', 'grant', 'group', 'having', 'hour', 'identity', 'immediate', 'in', 'indicator', 'initially', 'inner', 'input', 'insensitive', 'insert', 'int', 'integer', 'intersect', 'interval', 'into', 'is', 'isolation', 'join', 'keyPPPPP', 'language', 'last', 'leading', 'left', 'level', 'like', 'limit', 'list', 'local', 'lower', 'match', 'max', 'min', 'minute', 'missing', 'module', 'month', 'names', 'national', 'natural', 'nchar', 'next', 'no', 'not', 'null', 'nullif', 'numeric', 'octet_length', 'of', 'on', 'only', 'open', 'option', 'or', 'order', 'outer', 'output', 'overlaps', 'pad', 'partial', 'pivot', 'position', 'precision', 'prepare', 'preserve', 'primary', 'prior', 'privileges', 'procedure', 'public', 'read', 'real', 'references', 'relative', 'restrict', 'revoke', 'right', 'rollback', 'rows', 'schema', 'scroll', 'second', 'section', 'select', 'session', 'session_user', 'set', 'sexp', 'size', 'smallint', 'some', 'space', 'sql', 'sqlcode', 'sqlerror', 'sqlstate', 'string', 'struct', 'substring', 'sum', 'symbol', 'system_user', 'table', 'temporary', 'then', 'time', 'timestamp', 'timezone_hour', 'timezone_minute', 'to', 'trailing', 'transaction', 'translate', 'translation', 'trim', 'true', 'tuple', 'union', 'unique', 'unknown', 'unpivot', 'update', 'upper', 'usage', 'user', 'using', 'value', 'values', 'varchar', 'varying', 'view', 'when', 'whenever', 'where', 'with', 'work', 'write', 'year', 'zone']
-        self._session = boto3.client('s3')
+        self.__session = boto3.client('s3')
+        self.__allowed_extensions = ['csv', 'json', 'parquet', 'gz', 'bz2', 'snappy']
 
     def readData(self, key: str,
                  format: str,
@@ -20,30 +21,92 @@ class S3Synergy:
                  query: str = '',
                  lines: int=0,
                  compression: str = '',
-                 quoteChar: str = '') -> DataFrame:
+                 quoteChar: str = '',
+                 op_serialization: str = 'JSON') -> DataFrame:
         '''Read data in the required format and returns the dataframe'''
         if(format.lower() == 'csv'):
             if(query == ''):
                 return S3Synergy._get_csv_data(self, key, lines, sep, headers),None,None
             else:
                 query = S3Synergy._generate_comatible_SQL(self, query, header_flag, headers, sep, lines)
-                data, stats= S3Synergy._get_csv_data_query(self, key, query, sep, header_flag, compression, quoteChar)
+                data, stats= S3Synergy._get_csv_data_query(self, key, query, sep, header_flag, compression, quoteChar, op_serialization)
                 df = S3Synergy._jsonToDF(data, header_flag, headers)
                 return df, stats['BytesScanned'], stats['BytesProcessed']
 
         elif(format.lower() == 'json'):
-            data, stats = S3Synergy._get_data(self, key, query, lines, 'json')
-            df = S3Synergy._jsonToDF(data)
+            data, stats = S3Synergy._get_data(self, key, query, lines, 'json', op_serialization)
+            df = S3Synergy._jsonToDF(data,op_serialization)
             return df, stats['BytesScanned'], stats['BytesProcessed']
 
         elif(format.lower() == 'parquet'):
-            data, stats = S3Synergy._get_data(self, key, query, lines, 'parquet')
+            data, stats = S3Synergy._get_data(self, key, query, lines, 'parquet', op_serialization)
             # print(data,stats)
-            df = S3Synergy._jsonToDF(data)
-            return df, stats['BytesScanned'], stats['BytesProcessed']
-
+            if(len(data)==0):
+                return DataFrame(),stats['BytesScanned'], stats['BytesProcessed']
+            else:
+                df = S3Synergy._jsonToDF(data,op_serialization)
+                return df, stats['BytesScanned'], stats['BytesProcessed']
         else:
             raise InvalidFormatException('Invalid Format spotted.. Supported Types are csv, json or parquet')
+        
+    def readDataFromFolder(self,
+                           key: str,
+                           format: str,
+                           header_flag: str = 'Y',
+                           headers: str = None,
+                           sep: str = ',',
+                           query: str = '',
+                           lines: int=0,
+                           compression: str = '',
+                           quoteChar: str = '',
+                           op_serialization: str = 'CSV',
+                           wildcard: str = None) -> DataFrame:
+        files = S3Synergy._get_valid_files(self, key, wildcard)
+        runtime = 'start'
+        bucket,_,folder = key[5:].partition('/')
+        for file in files:
+            key2 = f's3://{bucket}/{file}'
+            arg = {'self':self,
+            'key': key2, 
+            'format': format, 
+            'header_flag': header_flag, 
+            'headers': headers, 
+            'sep': sep, 
+            'query': query, 
+            'lines': lines, 
+            'compression': compression, 
+            'quoteChar': quoteChar, 
+            'op_serialization': op_serialization}
+            if(runtime=='start'):
+                df, scanned, process = S3Synergy.readData(**arg)
+            else:
+                df2, scanned2, process2 = S3Synergy.readData(**arg)
+                df = df.append(df2)
+                scanned+=scanned2
+                process+=process2
+        return df, scanned, process
+
+
+    def _get_valid_files(self, path: str, wildcard: str):
+        s3 = self.__session
+        bucket,_,folder = path[5:].partition('/')
+        flag = True
+        kwargs = {'Bucket':bucket,'Prefix':folder}
+        while flag:
+            response = s3.list_objects_v2(**kwargs)
+            files = [i['Key'] for i in response['Contents'] if(i['Key'].rsplit('.',1)[1] in self.__allowed_extensions and i['Size']>0)]
+            try:
+                kwargs['ContinuationToken'] = response['NextContinuationToken']
+            except KeyError as E:
+                flag = False
+        if(wildcard is not None):
+            files = S3Synergy._filter_wildcards(files, wildcard)
+        return files
+    
+    def _filter_wildcards(files: List, wildcard: str) -> List:
+        wildcard = wildcard.strip().replace('*', '.+')+'$'
+        return [i for i in files if re.search(wildcard,i.rsplit('/')[1])]
+
 
     def _get_csv_data(self, key: str, lines: int, sep: str, header) -> DataFrame:
         data = S3Synergy._get_StringBufferFromS3(self, key, lines+1)
@@ -54,9 +117,8 @@ class S3Synergy:
         return df
 
     def _get_StringBufferFromS3(self, key: str, lines: int) -> List:
-        s3 = self._session
-        bucket,sp,fileName = key[5:].partition('/')
-        del(sp)
+        s3 = self.__session
+        bucket,_,fileName = key[5:].partition('/')
         response = s3.get_object(
             Bucket=bucket,
             Key=fileName
@@ -71,25 +133,28 @@ class S3Synergy:
                 break
         return dataList
                
-    def _get_data(self, key: str, query: str, lines: int, format: str):
-        bucket,sp,fileName = key[5:].partition('/')
-        del(sp)
+    def _get_data(self, key: str, query: str, lines: int, format: str, op_serialization: str):
+        bucket,_,fileName = key[5:].partition('/')
         input_serialization = {}
         if(format.upper()=='JSON'):
             input_serialization['JSON']={}
         elif(format.upper()=='PARQUET'):
             input_serialization['Parquet']={}
-        return S3Synergy._run_s3_select(self, bucket, fileName, query, input_serialization)
+        return S3Synergy._run_s3_select(self, bucket, fileName, query, input_serialization, op_serialization)
 
-    def _run_s3_select(self, bucket, key, query, input_serialization):
-        s3 = self._session
+    def _run_s3_select(self, bucket, key, query, input_serialization, op_serialization):
+        s3 = self.__session
+        if(op_serialization.upper()=='JSON'):
+            output_serialization = {'JSON':{'RecordDelimiter': ','}}
+        else:
+            output_serialization = {'CSV':{}}
         response = s3.select_object_content(
         Bucket = bucket,
         Key = key,
         ExpressionType = 'SQL',
         Expression = query,
         InputSerialization = input_serialization,
-        OutputSerialization = {'JSON': {'RecordDelimiter': ','}},
+        OutputSerialization = output_serialization,
         )
         records = []
         for event in response['Payload']:
@@ -99,10 +164,9 @@ class S3Synergy:
                 statsDetails = event['Stats']['Details']
         return ''.join(records), statsDetails
 
-    def _get_csv_data_query(self, key, query, sep, header, compression, quoteChar):
+    def _get_csv_data_query(self, key, query, sep, header, compression, quoteChar, op_serialization):
         '''To Be Implemented'''
-        bucket,sp,fileName = key[5:].partition('/')
-        del(sp)
+        bucket,_,fileName = key[5:].partition('/')
         header = 'Use' if header.upper()=='Y' and compression=='' else 'NONE'
         
         input_serialization = {'CSV' : {
@@ -114,7 +178,7 @@ class S3Synergy:
         if(compression!=''):
             input_serialization['CompressionType'] = compression.upper()
 
-        return S3Synergy._run_s3_select(self, bucket, fileName, query, input_serialization)
+        return S3Synergy._run_s3_select(self, bucket, fileName, query, input_serialization, op_serialization)
 
     def _generate_comatible_SQL(self, query, header_flag, headers, sep, lines) -> str:
         if(header_flag.upper() == 'Y'):
@@ -137,8 +201,12 @@ class S3Synergy:
             return query
 
     @staticmethod
-    def _jsonToDF(data: str, header_flag: str='N', headers = None) -> DataFrame:
-        df = DataFrame(json.loads('['+data.strip(',')+']'))
+    def _jsonToDF(data: str, op_serialization, header_flag: str='N', headers = None) -> DataFrame:
+        if(op_serialization.upper()=='JSON'):
+            df_data = json.loads('['+data.strip(',')+']')
+            df = DataFrame(df_data)
+        else:
+            df = read_csv(StringIO(data))
         if(headers is not None):
             headers = headers.split(',')
             df.columns=[headers[int(col.split('_')[1])-1] for col in df.columns.tolist()]
